637517d893ecb83fce630333791a00d5e2815dd7
Mateusz Pietrzak <mateusz@mzpk.dev>
2026-08-25 07:56:55 +0200

    build: prepare package for npm

--- dist/cli.js
+++
@@ -1,5 +0,0 @@
-import { t as setupSkyEmu } from "./src-D9NFthS-.js"
-//#region src/cli.ts
-await setupSkyEmu()
-//#endregion
-export {}
--- dist/index.js
+++
@@ -1,2 +0,0 @@
-import { n as skyEmuBinary, r as skyEmuDirectory, t as setupSkyEmu } from "./src-D9NFthS-.js"
-export { setupSkyEmu, skyEmuBinary, skyEmuDirectory }
